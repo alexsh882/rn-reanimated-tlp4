46c96b5e283fccb3940fb1e31bd001d6b0b40a04
alexsh882 <alexsh882@gmail.com>
2024-08-29 16:18:40 -0300

    chore: se agregaron comentarios de código.

--- app/index.tsx
+++ app/index.tsx
@@ -14,27 +14,34 @@ import Animated, {
 import { useEffect } from "react";
 
 export default function HomeScreen() {
-  const translateX = useSharedValue<number>(0);
 
+  // declaración de variable para la animación de título y display del título
+  const translateX = useSharedValue<number>(0);
   const displayTitle = useSharedValue<boolean>(true);
 
+  // declaración de variable para la animación de botón "Iniciar"
   const pressed = useSharedValue<boolean>(false);
 
+  // declaración de estilos animados para el título
   const animatedTitleStyles = useAnimatedStyle(() => ({
     transform: [{ translateY: withSpring(translateX.value * 2) }],
     opacity: withTiming(displayTitle.value ? 1 : 0),
   }));
 
+  // declaración de estilos para el fondo, color y animación.
   const animatedViewStyles = useAnimatedStyle(() => ({
     backgroundColor: pressed.value ? "#111B21" : "#202C33",
     transform: [{ scale: withTiming(1) }],
   }));
 
+
+  // función para manejar el evento de presionar el botón
   const handlePress = () => {
     pressed.value = !pressed.value;
     displayTitle.value = !displayTitle.value;
   };
 
+  // useEffect para iniciar la animación del título cuando se monta el componente.
   useEffect(() => {
     translateX.value = 250;
   }, []);
