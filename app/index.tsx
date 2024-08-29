a9c5be6b142cf287e6731ee801abd87f5eb7074c
alexsh882 <alexsh882@gmail.com>
2024-08-29 15:37:17 -0300

    feat: se agregó la funcionalidad al botón "iniciar" para cambiar el fondo con efecto FadingTransition

--- app/index.tsx
+++ app/index.tsx
@@ -2,19 +2,27 @@ import { StyleSheet, Button, View } from "react-native";
 
 import { HelloWave } from "@/components/HelloWave";
 import { ThemedText } from "@/components/ThemedText";
-import Animated, { useAnimatedStyle, useSharedValue, withSpring, withTiming  } from "react-native-reanimated";
+import Animated, { useAnimatedStyle, useSharedValue, withSpring, withTiming, FadingTransition  } from "react-native-reanimated";
 import { useEffect } from "react";
 
 export default function HomeScreen() {
   const translateX = useSharedValue<number>(0);
+
+  const pressed = useSharedValue<boolean>(false);
 
 
   const animatedTitleStyles = useAnimatedStyle(() => ({
     transform: [{ translateY: withSpring(translateX.value * 2) }],
   }));
 
+  const animatedViewStyles = useAnimatedStyle(() => ({
+    backgroundColor: pressed.value ? '#111B21' : '#202C33',
+    transform: [{ scale: withTiming(1) }],
+  }));
 
-  
+  const handlePress = () => {
+    pressed.value = !pressed.value;
+  };
 
   useEffect(() => {
     translateX.value = 250;
@@ -22,13 +30,13 @@ export default function HomeScreen() {
 
 
   return (
-    <Animated.View style={[styles.view]}>
+    <Animated.View layout={FadingTransition} style={[styles.view, animatedViewStyles]}>
       <Animated.View style={[styles.titleContainer, animatedTitleStyles ]}>        
         <ThemedText type="title">Bienvenidos!</ThemedText>
         <HelloWave />
       </Animated.View>
       <View style={styles.buttonContainerStyle}>
-        <Button title="Iniciar"  />
+        <Button title="Iniciar" onPress={handlePress} />
       </View>
     </Animated.View>
   );
